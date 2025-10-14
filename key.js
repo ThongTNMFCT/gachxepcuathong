65e7087dca2ed73afde7fb0ab6f8babd570040d2
Trần Ngọc Minh Thông <thongtnmfct31178@gmail.com>
2025-10-14 16:28:33 +0700

    Add files via upload

---
+++ key.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCrDBWFTuf7BmLQudAs8c-kmsO1jCJ48ME",
+  authDomain: "gac-xep-cua-thong.firebaseapp.com",
+  projectId: "gac-xep-cua-thong",
+  storageBucket: "gac-xep-cua-thong.appspot.com", 
+  messagingSenderId: "105625839337",
+  appId: "1:105625839337:web:918de1364711a30c5a3278"
+};
+
+export const IMGBB_API_KEY = "cc49b6a38299d2c8b719d23c67b0c6c3";
+
